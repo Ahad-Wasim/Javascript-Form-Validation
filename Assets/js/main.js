4a3f987294af6900f03cded32c48bbb6766512cd
Ahad Wasim <ahadwasim@gmail.com>
2015-03-06 11:35:06 -0800

    I have gotten th success message working

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -67,33 +67,64 @@ function load_page(page_url){
     });
     
 }
+
+
+
+
+
+
+
+
 function validate_contact(){
-    var contact_inputs = $("#contact_form input, #contact_form textarea");
-    console.log(contact_inputs);
+    var contact_inputs = $("#contact_form input, #contact_form textarea");                  // Grab the Contact Form Inputs + The TextArea and storing that in a variable
+    console.log(contact_inputs);                                                            
     //keep track of how many errors there are
-    var error_count = 0;
-    $("#contact_form .error_msg").remove();
-    $(contact_inputs).each(function(){
-        var str='';
-        var regex=null;
-        var error_msg = '';
+    var error_count = 0;                                                                    // If their is an Error. The MESSAGE WONT SEND. Start the Error at 0
+   
+    
+        $("#contact_form .error_msg").remove();                                             // if there is an error remove it
+    
+    $(contact_inputs).each(function(){                                                      // Iterate through the Contact Form Inputs + The TextArea 
+       
+        var str='';                                                     // the str val would hold the value inside the input. We would then compare that later on
+        
+        var regex=null;                                                                     // if regex is not vailid it will equal null
+        
+        var error_msg = '';                                                                 // error message will start off blank
+        
+        
+        
+        
+        
         //input_validation[$(this).attr('name')];  //beginnings of USING the object for input validation
-        switch($(this).attr('name')){
-                case 'name':
-                    str = $(this).val();
-                    regex = /[a-zA-Z]{3,}/;
-                    error_msg="Must be at least 3 characters long, no numbers or characters";
+        
+        
+        
+        
+        
+        
+        
+        
+        switch($(this).attr('name')){                                                         // if this inputs value .attribute = name
+                case 'name':                                                                  // if the attributes name == name then perform the bottom
+                   
+                str = $(this).val();                                                          //the variable str == the value
+                    regex = /[a-zA-Z]{3,}/;                                                   // the REGEX the str must equal to
+                    error_msg="Must be at least 3 characters long, no numbers or characters"; // if Regex does not match perform this part
                     break;
-                case 'email':
-                    str = $(this).val();
-                    regex = null;
-                    error_msg="";
+                
+                case 'email':                                                               // if the attributes name == email then perform the bottom
+                    str = $(this).val();                                                    //Grab the value of that and store it inside string
+                    regex = null;                                                           //were not trying to match the REGEX so it is all ready returning null
+                    error_msg="";                                                           // There is no error message
                     break;
-                case 'phone':
-                    str = $(this).val();
-                    regex = null;
-                    error_msg="";
+                
+                case 'phone':                                                               // if the attributes name == phone then perform the bottom
+                    str = $(this).val();                                                    //Grab the value of that and store it inside string
+                    regex = null;                                                           //it doesn't have any limitations                                     
+                    error_msg="";                                                           // there is no error
                     break;
+                
                 case 'subject':
                     str = $(this).val();
                     regex = null;
@@ -101,35 +132,61 @@ function validate_contact(){
                     break;
                 case 'comments':
                     str = $(this).val();
-                    regex = /.{3,250}/;
+                    regex = /.{3,250}/;                                                 //Their should be between 3,250 characters
                     error_msg="Comments can only be between 3 and 250 characters";
                     break;
                 default:
-        }
-        if(regex!==null){  //if there is a validation rule to check
+        } //This closes the switch statement
+        
+        
+        
+        
+        
+        if(regex!==null){                                                                   // if regex is equal to null   MEANING IT HAS EXITED THE SWITCH STATEMENT WITH AN ERROR
             if(str.match(regex)===null){ //if there was a validation error
                 //alert($(this).attr('name')+":"+error_msg); //send error message
-                var error_span = $("<span/>").addClass('error_msg').text(error_msg);
-                error_span.insertAfter($(this));
+                
+                var error_span = $("<span/>").addClass('error_msg').text(error_msg);        // make a variable create a span add a class and the error message inside the tex
+                error_span.insertAfter($(this));                                          // use jQuery insert after to insert the error_span after this
+                
                 //$(this).parent().find('.error_msg').html(error_msg);
-                error_count++;  //increase error count
+                
+                
+                error_count++;  //increase error count                                      // If it gets to this if statement that means we have an error
             }
             
             
-        }
-    });
+        }  // exit the if statement
+    });  // for the each function
+    
+    
     console.log("error count " +error_count);
-    if(error_count==0){  //if no errors
-        send_message();   //trigger our form submission function
+    if(error_count==0){                                                                     //if no errors
+        send_message();                                                                     //trigger our form submission function
     }
     else {
-        return false;  //return false, so that the form submit will fail
+        return false;                                                                       //return false, so that the form submit will fail
     }
     
 }
 
-function send_message(){  //dummy function for sending message
+var success_count = 0;
+function send_message(){  //dummy function for sending message                              // if it reaches this point then we have succesfully acheived our function
+    
+    if(success_count == 1){
+        console.log("This is the ",success_count);
+        return
+    }
+    
+    var succeed = $('<span>').text("Your message was sent successfully").css({color:'green', marginTop:'5px',border:'1px solid black'});
+    console.log(succeed);
+    
+    succeed.insertAfter('button');
     console.log('to be continued...');
+    
+    success_count ++
+    
+    
 }
 
 
