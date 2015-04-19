698fa2e58c4691c34a806f6956b3b26d38533d6a
Ahad Wasim <ahadwasim@gmail.com>
2015-04-18 21:55:31 -0700

    Learningfuze Javascript form Validation project complete

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,72 +1,46 @@
-var pages={
-    Welcome: 
-        {pageUrl : 'home.html', default:true},
-    "Our Macarons" : 
-        { pageUrl : 'macarons.html'},
-    "Gifts & Parties" : 
-        { pageUrl :'gifts-parties.html'},
-    Contact : 
-        { pageUrl : 'contact.html'}
+var pages = {
+    Welcome: {
+        pageUrl: 'home.html',
+        default: true
+    },
+    "Our Macarons": {
+        pageUrl: 'macarons.html'
+    },
+    "Gifts & Parties": {
+        pageUrl: 'gifts-parties.html'
+    },
+    Contact: {
+        pageUrl: 'contact.html'
+    }
 };
-
 //beginnings of an object for use with error validation
-
-
-
-
-
-
-
-
 //                  OBJECTS
-
 var input_validation = {
-    name: 
-        {
+    name: {
         regex: /[a-zA-Z]{3,}/,
-         error_msg:"Must be at least 3 characters long, no numbers or characters"
-        },
-    
-    email: 
-        {
+        error_msg: "Must be at least 3 characters long, no numbers or characters"
+    },
+    email: {
         regex: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
-         error_msg:"Must be at least 3 characters long, no numbers or characters"
-        },
-    
-    phone: 
-        {
+        error_msg: "Must be at least 3 characters long, no numbers or characters"
+    },
+    phone: {
         regex: /[a-zA-Z]{3,}/,
-         error_msg:"Must be at least 3 characters long, no numbers or characters"
-        },
-    
-    subject:
-        {
+        error_msg: "Must be at least 3 characters long, no numbers or characters"
+    },
+    subject: {
         regex: /.{3,50}/,
-        error_msg: "There is a problem",   
-        },
-    
-    comments:
-        {
+        error_msg: "There is a problem",
+    },
+    comments: {
         regex: /.{3,250}/,
-        error_msg:"There is a problem",
-        }
- 
+        error_msg: "There is a problem",
+    }
 };
-
-
 //                   OBJECTS
-
-
-
-
-
-
-
-
-$("document").ready(function(){
-
-//do stuff when the document is ready!
-/*$(".main-nav li a").each(function(){
+$("document").ready(function() {
+    //do stuff when the document is ready!
+    /*$(".main-nav li a").each(function(){
         //do something to each item found
         $(this).click(function(){
 
@@ -78,15 +52,15 @@ $("document").ready(function(){
     create_menu();
 });
 /*<li data-url="home.html"><a >Welcome</a></li>*/
-function create_menu(){
+function create_menu() {
     var main_nav_ul = $(".main-nav ul");
-    for(var index in pages){
-        console.log("The index is "+index);
+    for (var index in pages) {
+        console.log("The index is " + index);
         var li = $("<li/>");
         var a = $("<a/>").text(index);
-        (function(){
+        (function() {
             var my_index = index;
-            li.on("click","a",function(){
+            li.on("click", "a", function() {
                 var my_page = pages[my_index];
                 load_page(my_page.pageUrl);
             });
@@ -94,63 +68,29 @@ function create_menu(){
         li.append(a);
         //a.appendTo(li);
         main_nav_ul.append(li);
-        if(pages[index].default==true){
+        if (pages[index].default == true) {
             load_page(pages[index].pageUrl);
         }
     }
 }
 
-
-
-
-
-
-
-
-
-
-function load_page(page_url){
+function load_page(page_url) {
     //load the indicated page into the #main_content section
-    $.get(page_url,function(data){
+    $.get(page_url, function(data) {
         $("#main_content").html(data);
     });
-    
 }
 
-
-
-
-
-
-
-
-function validate_contact(){
-    var contact_inputs = $("#contact_form input, #contact_form textarea");                  // Grab the Contact Form Inputs + The TextArea and storing that in a variable
-    console.log(contact_inputs);                                                            
-    
-    var error_count = 0;                                                                    // If their is an Error. The MESSAGE WONT SEND. Start the Error at 0
-   
-    
-        $("#contact_form .error_msg").remove();                          // everytime we start this function we want to remove the current error. So the user does not see their previous error
-    
-    $(contact_inputs).each(function(){                                                      // Iterate through the Contact Form Inputs + The TextArea 
-       
-        var str = $(this).val();                                                             // this str is looping through all the inputs and will get the value 1 by 1.
-        
-        var regex = input_validation[$(this).attr('name')].regex;                            // It will go thru the input validation and get the currents inputs name and regex                      
-        
-        var error_msg = input_validation[$(this).attr('name')].error_msg;        // It will go thru the input validation and get the currents inputs name and error_msg                  
-        
-        
-        
-                                                                                                           
-                                                                                                       
-        
-        
-        
-
-        
-       /* switch($(this).attr('name')){                                                           // if this inputs value .attribute = name
+function validate_contact() {
+    var contact_inputs = $("#contact_form input, #contact_form textarea"); // Grab the Contact Form Inputs + The TextArea and storing that in a variable
+    console.log(contact_inputs);
+    var error_count = 0; // If their is an Error. The MESSAGE WONT SEND. Start the Error at 0
+    $("#contact_form .error_msg").remove(); // everytime we start this function we want to remove the current error. So the user does not see their previous error
+    $(contact_inputs).each(function() { // Iterate through the Contact Form Inputs + The TextArea 
+        var str = $(this).val(); // this str is looping through all the inputs and will get the value 1 by 1.
+        var regex = input_validation[$(this).attr('name')].regex; // It will go thru the input validation and get the currents inputs name and regex                      
+        var error_msg = input_validation[$(this).attr('name')].error_msg; // It will go thru the input validation and get the currents inputs name and error_msg                  
+        /* switch($(this).attr('name')){                                                           // if this inputs value .attribute = name
                 case 'name':                                                                    // if the attributes name == name then perform the bottom
                    
                     str = $(this).val();                                                        // the variable str == the value
@@ -184,76 +124,54 @@ function validate_contact(){
         } //This closes the switch statement
         
         */
-        
-        
-        
-        
-        if(regex!==null){                                                                   // if their is something inside the regex
-            if(str.match(regex)===null){ //if there was a validation error                  // match the regex. IF THEIRS A NULL THAT MEANS THE REGEX DOES NOT MATCH THE STR
-                
-                
-                var error_div = $("<div/>").addClass('error_msg alert alert-danger').text(error_msg);        // make a variable create a span add a class and the error message inside the tex
-                
-                error_div.insertAfter($(this));                                             // use jQuery insert after to insert the error_span after this
-                
-                var close_button = $("<button/>",{type:'button',class:'close',"data-dismiss":'alert','aria-label':'Close'});
-                
-                var span = $('<span>',{'aria-hidden':'true'}).html('&times;');
-                
+        if (regex !== null) { // if their is something inside the regex
+            if (str.match(regex) === null) { //if there was a validation error                  // match the regex. IF THEIRS A NULL THAT MEANS THE REGEX DOES NOT MATCH THE STR
+                var error_div = $("<div/>").addClass('error_msg alert alert-danger').text(error_msg); // make a variable create a span add a class and the error message inside the tex
+                error_div.insertAfter($(this)); // use jQuery insert after to insert the error_span after this
+                var close_button = $("<button/>", {
+                    type: 'button',
+                    class: 'close',
+                    "data-dismiss": 'alert',
+                    'aria-label': 'Close'
+                });
+                var span = $('<span>', {
+                    'aria-hidden': 'true'
+                }).html('&times;');
                 close_button.append(span);
-                
                 error_div.append(close_button);
                 //$(this).parent().find('.error_msg').html(error_msg);
-                
-                
-                error_count++;  //increase error count                                      // If it gets to this if statement that means we have an error
+                error_count++; //increase error count                                      // If it gets to this if statement that means we have an error
             }
-            
-            
-        }                        // exit the if statement
-        
-        
-    });                         // for the each function
-    
-    
-    
-    
-    
-    console.log("error count " +error_count);
-    if(error_count==0){                                                                     //  if no errors
-        send_message();                                                                     //  trigger our form submission function
+        } // exit the if statement
+    }); // for the each function
+    console.log("error count " + error_count);
+    if (error_count == 0) { //  if no errors
+        send_message(); //  trigger our form submission function
+    } else {
+        return false; //return false, so that the form submit will fail
     }
-    else {
-        return false;                                                                       //return false, so that the form submit will fail
-    }
-    
 }
-
 var success_count = 0;
-function send_message(){  //dummy function for sending message                              // if it reaches this point then we have succesfully acheived our function
-    
-    if(success_count == 1){
-        console.log("This is the ",success_count);
+
+function send_message() { //dummy function for sending message                              // if it reaches this point then we have succesfully acheived our function
+    if (success_count == 1) {
+        console.log("This is the ", success_count);
         return
     }
-    
     var succeed = $('<div>').text("Your message was sent successfully").addClass('alert alert-success');
-    var close_button = $("<button/>",{type:'button',class:'close',"data-dismiss":'alert','aria-label':'Close'});
-    var span = $('<span>',{'aria-hidden':'true'}).html('&times;');
+    var close_button = $("<button/>", {
+        type: 'button',
+        class: 'close',
+        "data-dismiss": 'alert',
+        'aria-label': 'Close'
+    });
+    var span = $('<span>', {
+        'aria-hidden': 'true'
+    }).html('&times;');
     close_button.append(span);
     succeed.append(close_button);
-    
-    
     console.log(succeed);
-    
     succeed.insertAfter('button');
     console.log('to be continued...');
-    
-    success_count ++
-    
-    
+    success_count++
 }
-
-
-
-
